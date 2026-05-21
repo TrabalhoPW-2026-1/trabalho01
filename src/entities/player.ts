675f665b75b4cf10b287de78cc7b3631bfc318ad
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2026-05-21 09:14:07 -0400

    feat: implementar classe Ship como Entidade

---
+++ src/entities/player.ts
@@ -0,0 +1,47 @@
+import { TAMX } from "../config.js";
+import { Entity } from "../core/entity.js";
+import { space } from "../space.js";
+
+const directions = [
+  "assets/png/playerLeft.png",
+  "assets/png/player.png",
+  "assets/png/playerRight.png",
+];
+
+export class Ship implements Entity {
+
+  element: HTMLImageElement;
+
+  position = {
+    x: TAMX / 2 - 50,
+    y: 700
+  };
+
+  velocity = {
+    x: 0,
+    y: 0
+  };
+
+  direction = 1;
+
+  constructor() {
+    this.element = document.createElement("img");
+
+    this.element.id = "ship";
+    this.element.src = directions[this.direction];
+
+    this.syncElement();
+
+    space.element.appendChild(this.element);
+  }
+
+  syncElement() {
+    this.element.style.left = `${this.position.x}px`;
+    this.element.style.top = `${this.position.y}px`;
+  }
+
+  setDirection(direction: number) {
+    this.direction = direction;
+    this.element.src = directions[this.direction];
+  }
+}
